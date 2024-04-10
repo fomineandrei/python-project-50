ab701525a09b2c2b36f5818eabcd04b4613a4035
fomineandrei <fomineandrei@yandex.ru>
2024-04-10 14:28:55 +0300

    modified: gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,7 @@ def help_func():
     parser = argparse.ArgumentParser(prog="gendiff", description="Compares two configuration files and shows a difference.")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", dest="FORMAT")
     args = parser.parse_args()
     return args.first_file, args.second_file
 
