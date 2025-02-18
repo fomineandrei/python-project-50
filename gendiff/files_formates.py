2ccc2dee13ea7fab132d683b61363e60d83d5121
fomineandrei <fomineandrei@yandex.ru>
2025-02-18 12:51:35 +0300

    added .yml formate

--- gendiff/files_formates.py
+++ gendiff/files_formates.py
@@ -1,25 +1,41 @@
 import json
 import yaml
-from yaml.loader import SafeLoader
+from yaml.loader import BaseLoader
+
+
+# From Python to JSON decoder dictionary(bool and None data formate)
+JSON_TO_PYTHON_DECODER = {
+    None: 'null',
+    True: 'true',
+    False: 'false'
+}
+
+
+def decode_func(dict_):
+    for key, value in dict_.items():
+        if not isinstance(value, bool | None):
+            continue
+        else:
+            dict_[key] = JSON_TO_PYTHON_DECODER[value]
+    return dict_
 
 
 def json_to_dict(file_path: str):
-    file_ = open(file_path, 'r')
-    json_dict = json.load(file_)
-    file_.close()
+    with open(file_path, 'r') as f:
+        json_dict = json.load(f, object_hook=decode_func)
     return json_dict
 
 
 def yaml_to_dict(file_path: str):
-    file_ = open(file_path, 'r')
-    yaml_dict = yaml.load(file_, Loader=SafeLoader)
-    file_.close()
+    with open(file_path, 'r') as f:
+        yaml_dict = yaml.load(f, Loader=BaseLoader)
     return yaml_dict
 
 
 FILES_FORMATES = {
     'json': json_to_dict,
     'yaml': yaml_to_dict,
+    'yml': yaml_to_dict
 }
 
 
