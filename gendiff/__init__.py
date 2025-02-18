b9aec6582464a52ed35b56325cf3215c812a5dff
fomineandrei <fomineandrei@yandex.ru>
2025-02-18 12:49:55 +0300

    stylish deleted from output_formates/__init__.py and added to /__init__.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,9 +1,11 @@
 from gendiff.diff_gen import generate_diff
 from gendiff.cli import parse_func
 from gendiff.files_formates import file_to_dict
+from gendiff.output_formates.stylish import stylish
 
 __all__ = (
     'generate_diff',
     'parse_func',
     'file_to_dict',
+    'stylish'
 )
