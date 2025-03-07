27b910f98595019f5e4bb24b567f0df51fa5646d
fomineandrei <fomineandrei@yandex.ru>
2025-03-07 11:49:20 +0300

    refactored imported functions

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,12 +1,18 @@
 from gendiff.diff_gen import generate_diff
 from gendiff.cli import parse_func
 from gendiff.files_formates import file_to_dict
-from gendiff.output_formates.stylish import stylish
+from gendiff.output_formates import (
+    stylish,
+    plane,
+    json
+)
 
 
 __all__ = (
     'generate_diff',
     'parse_func',
     'file_to_dict',
-    'stylish'
+    'stylish',
+    'plane',
+    'json'
 )
--- gendiff/output_formates/__init__.py
+++ gendiff/output_formates/__init__.py
@@ -1,10 +1,12 @@
 from gendiff.output_formates.stylish import stylish
 from gendiff.output_formates.plane import plane
+from gendiff.output_formates.json_formate import json
 
 
 OUTPUT_FORMATES = {
     'stylish': stylish,
-    'plane': plane
+    'plane': plane,
+    'json': json
 }
 
 
