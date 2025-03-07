d6444764abd15badbeb3dc07fc065959980f2e59
fomineandrei <fomineandrei@yandex.ru>
2025-03-07 11:53:17 +0300

    fixed from json and yaml to python values transformations from files

--- gendiff/files_formates.py
+++ gendiff/files_formates.py
@@ -1,34 +1,17 @@
 import json
 import yaml
-from yaml.loader import BaseLoader
-
-
-# From Python to JSON decoder dictionary(bool and None data formate)
-JSON_TO_PYTHON_DECODER = {
-    None: 'null',
-    True: 'true',
-    False: 'false'
-}
-
-
-def decode_func(dict_):
-    for key, value in dict_.items():
-        if not isinstance(value, bool | None):
-            continue
-        else:
-            dict_[key] = JSON_TO_PYTHON_DECODER[value]
-    return dict_
+from yaml.loader import SafeLoader
 
 
 def json_to_dict(file_path: str):
     with open(file_path, 'r') as f:
-        json_dict = json.load(f, object_hook=decode_func)
+        json_dict = json.load(f)
     return json_dict
 
 
 def yaml_to_dict(file_path: str):
     with open(file_path, 'r') as f:
-        yaml_dict = yaml.load(f, Loader=BaseLoader)
+        yaml_dict = yaml.load(f, Loader=SafeLoader)
     return yaml_dict
 
 
