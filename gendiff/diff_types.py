020dbddf9dc0dffa48f70b6a7809ec1332c50134
fomineandrei <fomineandrei@yandex.ru>
2025-03-20 10:03:15 +0300

    refactored gendiff/diff_types.py

--- gendiff/diff_types.py
+++ gendiff/diff_types.py
@@ -1,27 +1,12 @@
-DIFF_DICT = {
-    (True, True, True): 'equal',
-    (True, True, False): 'update',
-    (True, False, False): 'deleted',
-    (False, True, False): 'added',
-}
-
-
-def is_key_in(dict_data, key):
-    return key in dict_data
-
-
-def is_values_equal(data1_value, data2_value):
-    return data1_value == data2_value
-
-
-def make_diff_key(dict1, dict2, key):
-    return (
-        is_key_in(dict1, key),
-        is_key_in(dict2, key),
-        is_values_equal(dict1.get(key), dict2.get(key)),
-    )
-
-
 def make_diff(data1_dict, data2_dict, key):
-    diff_key = make_diff_key(data1_dict, data2_dict, key)
-    return DIFF_DICT[diff_key]
+    key_in_data1 = key in data1_dict
+    key_in_data2 = key in data2_dict
+    is_equal = data1_dict.get(key) == data2_dict.get(key)
+    if key_in_data1 and not key_in_data2:
+        return 'deleted'
+    elif not key_in_data1 and key_in_data2:
+        return 'added'
+    elif key_in_data1 and key_in_data2 and is_equal:
+        return 'equal'
+    elif key_in_data1 and key_in_data2 and not is_equal:
+        return 'update'
